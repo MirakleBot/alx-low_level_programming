06ed8488105a156845806a0f16234d93a2d69971
MirakleBot <iheukwumereokoro@gmail.com>
2022-03-12 20:54:59 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 
 /**
- * main - prints "and that piece of art is useful"
+ * main - Entry point
  * Return: 1 if success
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n". 59);
 	return (1);
+}
