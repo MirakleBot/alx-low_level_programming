8a32bf83aff0f01972599c511cf87be972621d09
MirakleBot <iheukwumereokoro@gmail.com>
2022-03-16 08:35:50 -0700

    pass

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets in lowercase
+ * Return: o
+ */
+
+void print_alphabet(void)
+{
+	int alp = 'a';
+
+	while (alp <= 'z')
+	{
+		putchar('alp');
+		alp++;
+	}
+	putchar('\n');
+	return (0);
+}
