a4c7353a8d60c65f95e28bfc649e778556e9df7c
MirakleBot <iheukwumereokoro@gmail.com>
2022-03-22 16:27:22 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints all character in a string
+ * @str: string char to be printed
+ */
+
+void puts2(char *str)
+{
+	int k, i;
+
+	k = 0;
+	while (str[k] != '\0')
+	{
+		k++
+	}
+	for (i = 0; i < k; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
